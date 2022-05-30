0d8bb4b34f2c17ae96052965ef61c6806e123d27
KCampbell22 <ksoup12@gmail.com>
2022-05-29 21:20:35 -0500

    push to update before deploy on netlify

---
+++ app.js
@@ -0,0 +1,16 @@
+let sounds = [
+sounds[0] = loadSound("assets/sounds/boom.wav"),
+sounds[1] = loadSound("assets/sounds/clap.wav"),
+sounds[2] = loadSound("assets/sounds/hihat.wav"),
+sounds[3] = loadSound("assets/sounds/kick.wav"),
+sounds[4] = loadSound("assets/sounds/openhat.wav"),
+sounds[5] = loadSound("assets/sounds/ride.wav"),
+sounds[6] = loadSound("assets/sounds/snare.wav"),
+sounds[7] = loadSound("assets/sounds/tink.wav"),
+sounds[8] = loadSound("assets/sounds/tom.wav"), 
+sounds[9] = loadSound("assets/sounds/tom.wav"), 
+sounds[10] = loadSound("audio/0.mp3") ];
+
+
+
+
